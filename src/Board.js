458fd49a615567ad1e5f5d5b1ce688de2b9a2136
6y6a <a.gubin@leningrad.io>
2019-02-20 22:49:24 +0300

    смена игроков

--- src/Board.js
+++ src/Board.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Square from './Square'
 
 class Board extends React.Component {
-    state = {squares: Array(9).fill(null)}
+    state = {
+        squares: Array(9).fill(null),
+        xIsNext: true
+    }
 
     handleClick(i) {
         const squares = this.state.squares.slice()
-        squares[i] = 'X'
-        this.setState({squares})
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({squares, xIsNext: !this.state.xIsNext})
     }
 
     renderSquare(i) {
@@ -20,7 +23,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'New player: X';
+        const status = 'New player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
